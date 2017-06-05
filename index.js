2f493277cb1cb62f0b0e225f9a397e4dc94a83d0
emilywritescode <youallwhylime@gmail.com>
2017-06-04 20:14:57 -0400

    fixed navbar: sticky smooth transition

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     
     $(window).scroll(function () {
-        if ($(window).scrollTop() > $(window).height()) {
+        if ($(window).scrollTop() > $(window).height()-60) {
             $(".navigation").addClass("fixed-nav");
         } else {
             $(".navigation").removeClass("fixed-nav");
